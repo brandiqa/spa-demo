e43edde0075240c08790b68a9325f26e717d536e
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-12 17:11:28 +0300

    Basic page

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
-window.addEventListener('load', async ()=> {
-  // Instantiate api handler
-  const service = axios.create({
-    baseURL: 'http://localhost:35729/api',
-    timeout: 5000,
-  })
+window.addEventListener('load', ()=> {
 
-  // Load Currency Rates
-  const response = await service.get('/rates');
-  const rates = response.data.rates;
+  const showRates = async() => {
+    // Instantiate api handler
+    const service = axios.create({
+      baseURL: 'http://localhost:35729/api',
+      timeout: 5000,
+    })
 
-  // Display Rates Table
-  const rates_template = Handlebars.compile($('#rates-template').html());
-  const html = rates_template({ 'rates': rates });
-  $('#app').html(html);
+    // Load Currency Rates
+    const response = await service.get('/rates');
+    const rates = response.data.rates;
+
+    // Display Rates Table
+    const rates_template = Handlebars.compile($('#rates-template').html());
+    const html = rates_template({ 'rates': rates });
+    $('#app').html(html);
+  }
+
+  page.base('/');
+
+  page('/', showRates);
+  page();
 });
