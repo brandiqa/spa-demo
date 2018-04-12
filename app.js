4300e6678ffffcbb6531dbb48816d6b7382c4d44
Michael Wanyoike <micwanyoike@gmail.com>
2018-04-12 12:19:13 +0300

    Implementec client fetch for rates

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+window.addEventListener('load', async ()=> {
+  // Instantiate api handler
+  const service = axios.create({
+    baseURL: 'http://localhost:35729/api',
+    timeout: 5000,
+  })
+
+  // Load Currency Rates
+  const response = await service.get('/rates');
+  console.log(response.data.rates)
+
+});
